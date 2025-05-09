ba9561733645d536f732c8bc061f958d9f097e1e
Scott Frazer <scott.frazer@iongroup.com>
2025-05-09 14:29:33 -0400

    change certificate name to ARN

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "alb" {
-  source  = "https://github.com/ION-Analytics/terraform-bsg-alb.git"
+  source  = "ssh://git@github.com:ION-Analytics/terraform-bsg-alb.git"
 
   name   = format("%s-%s-router", var.env, var.component)
   vpc_id = var.platform_config["vpc"]
